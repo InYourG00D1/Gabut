7a21fed4554b8130c4550ed8c6523d9efa78c29e
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-09-05 23:33:12 +0700

    Add files via upload

---
+++ Gabut.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+	int num3;
+	string name;
+	cout << "Ya maap Baru bisa ini Aink Kan nuub \n\n" << endl;
+	cout << "Ragil-P\n" << endl;
+	cout << "InYourG00D\n" << endl;
+	cin >> name;
+	cout << "hae Salken " << name << endl;
+	cout << "Boleh Minta Pap T*? " << endl;
+	cin >> num1;
+	cout << "hmm boleh ko" << endl;
+	cout << "Tunggu Sebentar Ya" << endl;
+	cin >> num2;
+	cout << "Oke siap" << endl;
+	cout << "Wih Buruan Ya " << endl;
+	cin >> num3;
+	cout << "Sabar ajg !!" << endl;
+    if (num1==num2 + num3){
+        cout << "Sabar Bngst!" << endl;
+
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
